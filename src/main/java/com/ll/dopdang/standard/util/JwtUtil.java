096a8588c7b06562aaa818bdda3a3714584d0ca7
sungyeong98 <am25227903@gmail.com>
2025-03-30 16:35:44 +0900

    Feat: standard 패키지 생성 - standard 패키지 생성 - JwtUtil 클래스 및 기본 메서드 생성

---
+++ src/main/java/com/ll/dopdang/standard/util/JwtUtil.java
@@ -0,0 +1,68 @@
+package com.ll.dopdang.standard.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JwtUtil {
+	private final SecretKey secretKey;
+
+	public JwtUtil(@Value("${jwt.secret}") String secret) {
+		this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+			Jwts.SIG.HS512.key().build().getAlgorithm());
+	}
+
+	public Long getUserId(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("id", Long.class);
+	}
+
+	public String getUsername(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("username", String.class);
+	}
+
+	public String getRole(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("role", String.class);
+	}
+
+	public boolean isExpired(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration()
+			.before(new Date());
+	}
+
+	public Date getExpirationDate(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration();
+	}
+}
