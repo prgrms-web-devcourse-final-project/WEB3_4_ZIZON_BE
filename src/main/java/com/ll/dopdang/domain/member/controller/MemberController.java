7bf7643ab3475861e6c81728b473d35bf26927a8
sungyeong98 <am25227903@gmail.com>
2025-03-31 14:30:31 +0900

    Feat: 일반 회원가입 기능 구현 - 일반 회원가입 구현   - 이메일 양식 검사   - 비밀번호 8자 이상 + 특수문자 포함 검사   - 이름 최대 10글자 검사   - 전화번호 인증 로직 도입

--- src/main/java/com/ll/dopdang/domain/member/controller/MemberController.java
+++ src/main/java/com/ll/dopdang/domain/member/controller/MemberController.java
@@ -1,8 +1,17 @@
 package com.ll.dopdang.domain.member.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ll.dopdang.domain.member.dto.request.MemberSignupRequest;
+import com.ll.dopdang.domain.member.service.MemberService;
+import com.ll.dopdang.global.sms.dto.SmsVerificationResponse;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -17,4 +26,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class MemberController {
+	/**
+	 * 유저 서비스
+	 */
+	private final MemberService memberService;
+
+	/**
+	 * 회원 가입 메서드
+	 * @param req 회원가입 dto
+	 * @param code 인증 코드
+	 * @return {@link ResponseEntity}
+	 */
+	@PostMapping("/signup")
+	public ResponseEntity<Object> signup(
+		@Valid @RequestBody MemberSignupRequest req,
+		@RequestParam String code) {
+		if (code.equals("request")) {
+			SmsVerificationResponse resp = memberService.sendCode(req.getPhone());
+			return ResponseEntity.ok(resp);
+		}
+		memberService.signup(req, code);
+		return ResponseEntity.ok("회원 가입 성공");
+	}
 }
--- src/main/java/com/ll/dopdang/domain/member/service/MemberService.java
+++ src/main/java/com/ll/dopdang/domain/member/service/MemberService.java
@@ -1,7 +1,19 @@
 package com.ll.dopdang.domain.member.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.dopdang.domain.member.dto.request.MemberSignupRequest;
+import com.ll.dopdang.domain.member.entity.Member;
+import com.ll.dopdang.domain.member.entity.MemberRole;
+import com.ll.dopdang.domain.member.entity.MemberStatus;
+import com.ll.dopdang.domain.member.repository.MemberRepository;
+import com.ll.dopdang.global.redis.repository.RedisRepository;
+import com.ll.dopdang.global.sms.dto.SmsVerificationRequest;
+import com.ll.dopdang.global.sms.dto.SmsVerificationResponse;
+import com.ll.dopdang.global.sms.service.SmsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +24,74 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
+	/**
+	 * 유저 레포지터리
+	 */
+	private final MemberRepository memberRepository;
+	/**
+	 * 비밀번호 인코더
+	 */
+	private final PasswordEncoder passwordEncoder;
+	/**
+	 * redis 레포지터리
+	 */
+	private final RedisRepository redisRepository;
+	/**
+	 * 문자 발송 서비스
+	 */
+	private final SmsService smsService;
+	/**
+	 * ObjectMapper
+	 */
+	private final ObjectMapper objectMapper;
+
+	/**
+	 * 회원가입 메서드
+	 * @param req 회원가입 dto
+	 * @param code 인증 코드
+	 */
+	@Transactional
+	public void signup(MemberSignupRequest req, String code) {
+		memberRepository.findByEmail(req.getEmail()).ifPresent(m -> {
+			throw new IllegalArgumentException("이미 가입된 이메일 입니다.");
+		});
+
+		boolean isVerified = verify(req.getPhone(), code);
+		if (!isVerified) {
+			throw new RuntimeException("전화번호 인증에 실패하였습니다.");
+		}
+
+		Member member = Member.builder()
+			.email(req.getEmail())
+			.password(passwordEncoder.encode(req.getPassword()))
+			.name(req.getName())
+			.phone(req.getPhone())
+			.profileImage("")
+			.status(MemberStatus.ACTIVE.toString())
+			.userRole(MemberRole.ROLE_CLIENT.toString())
+			.memberId(req.getEmail())
+			.build();
+		memberRepository.save(member);
+		redisRepository.remove("VERIFIED_PHONE:" + req.getPhone());
+	}
+
+	/**
+	 * 인증 코드 검증 메서드
+	 * @param phone 전화번호
+	 * @param code 인증번호
+	 * @return {@link Boolean}
+	 */
+	public boolean verify(String phone, String code) {
+		SmsVerificationRequest req = new SmsVerificationRequest(phone, code);
+		return smsService.verifyCode(req);
+	}
+
+	/**
+	 * 인증번호 발송 메서드
+	 * @param phone 전화번호
+	 * @return {@link SmsVerificationResponse}
+	 */
+	public SmsVerificationResponse sendCode(String phone) {
+		return smsService.sendVerificationCode(phone);
+	}
 }
