c182f803d2c7976707682e86230cb3e6e4f93869
rOyalFruit <ekdeon@gmail.com>
2025-04-04 13:39:57 +0900

    style: 줄 바꿈 기호 통일 (CRLF -> LF)

--- src/main/java/com/ll/dopdang/domain/chatroom/entity/ChatRoom.java
+++ src/main/java/com/ll/dopdang/domain/chatroom/entity/ChatRoom.java
@@ -1,12 +1,16 @@
 package com.ll.dopdang.domain.chatroom.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
