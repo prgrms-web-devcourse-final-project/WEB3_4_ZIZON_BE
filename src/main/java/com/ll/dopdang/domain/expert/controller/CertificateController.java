2a60bb984ffc26d45206e0075c418c8bdd6108c2
quiemew0 <quiemew@gmail.com>
2025-04-06 03:00:06 +0900

    Feat : 전체 자격증 조회

--- src/main/java/com/ll/dopdang/domain/expert/controller/CertificateController.java
+++ src/main/java/com/ll/dopdang/domain/expert/controller/CertificateController.java
@@ -1,9 +1,14 @@
 package com.ll.dopdang.domain.expert.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ll.dopdang.domain.expert.dto.response.CertificateResponseDto;
 import com.ll.dopdang.domain.expert.service.CertificateService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,8 +19,17 @@ import lombok.RequiredArgsConstructor;
 public class CertificateController {
 	private final CertificateService certificateService;
 
-	// GET 요청으로 데이터 수집 및 저장 테스트
+	/**
+	 * 모든 Certificate 데이터 조회 API
+	 */
 	@GetMapping
+	public ResponseEntity<List<CertificateResponseDto>> getAllCertificates() {
+		List<CertificateResponseDto> certificates = certificateService.getAllCertificates();
+		return new ResponseEntity<>(certificates, HttpStatus.OK);
+	}
+
+	// GET 요청으로 데이터 수집 및 저장 테스트
+	@GetMapping("/onlyForTest")
 	public String fetchAndSaveCertificates() {
 		try {
 			certificateService.fetchAndSaveCertificates(); // 서비스 호출
