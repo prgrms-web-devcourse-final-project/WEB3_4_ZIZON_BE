27acca2804b2ae118f9899542bf1116bafab2b39
sungyeong98 <am25227903@gmail.com>
2025-04-08 10:25:33 +0900

    Feat: 로그인 response 항목 추가 - 로그인 시 보여지는 항목 추가   - id   - 이메일   - 이름   - 프로필사진   - 유저 상태   - expert_id

--- src/main/java/com/ll/dopdang/domain/member/dto/response/LoginResponse.java
+++ src/main/java/com/ll/dopdang/domain/member/dto/response/LoginResponse.java
@@ -1,24 +1,33 @@
 package com.ll.dopdang.domain.member.dto.response;
 
+import com.ll.dopdang.domain.member.entity.Member;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 /**
  * 로그인 시, 보여지는 데이터 dto
  */
 @Getter
+@Builder
 @AllArgsConstructor
 public class LoginResponse {
-	/**
-	 * 이름
-	 */
+	private Long id;
 	private String name;
-	/**
-	 * 이메일
-	 */
 	private String email;
-	/**
-	 * 프로필 사진
-	 */
 	private String profileImage;
+	private String status;
+	private Long expertId;
+
+	public static LoginResponse of(Member member) {
+		return LoginResponse.builder()
+			.id(member.getId())
+			.name(member.getName())
+			.email(member.getEmail())
+			.profileImage(member.getProfileImage())
+			.status(member.getStatus())
+			.expertId(member.getExpert() != null ? member.getExpert().getId() : null)
+			.build();
+	}
 }
