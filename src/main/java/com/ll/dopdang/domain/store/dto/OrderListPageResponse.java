76a0a2042162693001b00d5bb91f4b2de3cb4a96
rOyalFruit <ekdeon@gmail.com>
2025-04-15 09:50:09 +0900

    refactor: OrderListPageResponse 클래스를 record로 변환

--- src/main/java/com/ll/dopdang/domain/store/dto/OrderListPageResponse.java
+++ src/main/java/com/ll/dopdang/domain/store/dto/OrderListPageResponse.java
@@ -2,27 +2,13 @@ package com.ll.dopdang.domain.store.dto;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class OrderListPageResponse {
-	private List<OrderResponse> orders;
-	private int currentPage;
-	private int pageSize;
-	private boolean hasNext;
-
+public record OrderListPageResponse(
+	List<OrderResponse> orders,
+	int currentPage,
+	int pageSize,
+	boolean hasNext
+) {
 	public static OrderListPageResponse of(List<OrderResponse> orders, int currentPage, int pageSize, boolean hasNext) {
-		return OrderListPageResponse.builder()
-			.orders(orders)
-			.currentPage(currentPage)
-			.pageSize(pageSize)
-			.hasNext(hasNext)
-			.build();
+		return new OrderListPageResponse(orders, currentPage, pageSize, hasNext);
 	}
 }
